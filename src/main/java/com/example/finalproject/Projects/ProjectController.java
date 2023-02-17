0970a9109b0f5c80dbdd622a1fb8650d830730c2
Marcin <motyl1806@gmail.com>
2023-02-17 02:16:42 +0100

    Dodano usuwanie projektu, poprawiono wcześniejsze błędy wyświetlania

---
+++ src/main/java/com/example/finalproject/Projects/ProjectController.java
@@ -0,0 +1,74 @@
+package com.example.finalproject.Projects;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+
+@RequestMapping("/projects")
+public class ProjectController {
+
+    private final ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+
+        this.projectRepository = projectRepository;
+    }
+
+    // TODO: @Secured("ROLE_PROJECTS_TAB")
+    @GetMapping("/index")
+    ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("projects/index");
+
+        modelAndView.addObject("projects", projectRepository.findAll());
+
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    ModelAndView create() {
+        ModelAndView modelAndView = new ModelAndView("projects/create");
+
+        Project project = new Project();
+        modelAndView.addObject("project", project);
+
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    String save(@ModelAttribute Project project) {
+
+        boolean isNew = project.getId() == null;
+
+        projectRepository.save(project);
+
+        if (isNew) {
+            return "redirect:/projects/index";
+        } else {
+            return "redirect:/projects/edit/" + project.getId();
+        }
+    }
+
+    @GetMapping("/edit/{id}")
+    ModelAndView edit(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("projects/create");
+
+        Project project = projectRepository.findById(id).orElse(null);
+
+        modelAndView.addObject("project", project);
+
+        return modelAndView;
+
+    }
+
+@RequestMapping("/remove/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+            projectRepository.deleteById(id);
+            redirectAttributes.addFlashAttribute("messeage", "Projekt został usunięty");
+        return "redirect:/projects/index";
+    }
+
+    }
